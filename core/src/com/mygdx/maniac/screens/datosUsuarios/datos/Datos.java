c6a25bedcf4a4e1924b1897456e19d80a4302373
Eduardo <eduardocastillogimenez1@gmail.com>
2021-08-18 11:05:44 -0400

    Se limpiaron carpetas que n ose usan

--- core/src/com/mygdx/maniac/screens/datosUsuarios/datos/Datos.java
+++
@@ -1,9 +0,0 @@
-package com.mygdx.maniac.screens.datosUsuarios.datos;
-
-public class Datos {
-    public String nombre="";
-    public int nivelesSuperados=0;
-    int nii=3;
-    public Datos() {}
-
-}
